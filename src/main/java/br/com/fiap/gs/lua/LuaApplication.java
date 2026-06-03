2b031db287b72b5ae73bb846f1a24ac30fa1f6a3
Enzo Fatobene <enzoamafatobene@gmail.com>
2026-06-02 21:28:56 -0300

    Criacao do Projeto Lua

---
+++ src/main/java/br/com/fiap/gs/lua/LuaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gs.lua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/gs/lua/LuaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gs.lua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
